0f436c0d2018ef1f82afdbdec2c7ac23d2cd1b26
DtCarrot <darrenong53@gmail.com>
2018-10-16 10:43:26 +0800

    Changed port back to 5000. We will be using nginx instead

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
 const app = express()
-const port = 80
+const port = 3000 
 app.get('/', (req, res) => res.send('Hello World'))
 app.listen(port, () => console.log(`App running on ${port}`))
