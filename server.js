6436e418ebc932f67f2951a274e461ca80f58e7e
DtCarrot <darrenong53@gmail.com>
2018-10-16 10:42:24 +0800

    Changed code to run on port 80

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
 const app = express()
-const port = 3000
+const port = 80
 app.get('/', (req, res) => res.send('Hello World'))
 app.listen(port, () => console.log(`App running on ${port}`))
